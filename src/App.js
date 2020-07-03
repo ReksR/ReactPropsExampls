8257908f9a5b595c6d7df9bd3b2d7ee3d3be8c2d
devopsrekha <61942349+devopsrekha@users.noreply.github.com>
2020-07-04 01:16:38 +0530

    Class replacing class with function

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from "react";
 import Ans from "./Ans";
 import db from "./db";
-
+/*
 function App() {
   const den = db.map(questions => <Ans que={questions} ans={questions} />);
   return <div>{den}</div>;
 }
 
 export default App;
+*/
+
+class App extends React.Component {
+  render() {
+    const den = db.map(questions => <Ans que={questions} ans={questions} />);
+    return <div>{den}</div>;
+  }
+}
