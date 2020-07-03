a0454e0ae7fe8b1b189c6885965b4f2b01f421de
devopsrekha <61942349+devopsrekha@users.noreply.github.com>
2020-07-04 00:55:46 +0530

    Props changed the datas are taken directly from the database

--- src/Ans.js
+++ src/Ans.js
@@ -3,8 +3,12 @@ import React from "react";
 function Ans(props) {
   return (
     <div>
-      <h1 style={{ display: !props.que && "none" }}>Question:{props.que}</h1>
-      <h2 style={{ color: !props.que && "purple" }}>Answer: {props.ans} </h2>
+      <h1 style={{ display: !props.que.question && "none" }}>
+        Question:{props.que.question}
+      </h1>
+      <h2 style={{ color: !props.que.question && "purple" }}>
+        Answer: {props.ans.answer}{" "}
+      </h2>
       <hr />
     </div>
   );
